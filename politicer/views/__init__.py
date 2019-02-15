6e850a00d64fbb82bb625990ab1856b1931c79ab
richiemounti <babtdaddy67@gmail.com>
2019-02-15 07:33:47 +0300

    update on inputs

--- politicer/views/__init__.py
+++ politicer/views/__init__.py
@@ -8,6 +8,8 @@ class Views(object):
     @staticmethod
     def get_data():
         data = request.get_json()
+        if data is not request.get_json:
+            data = request.get_json(force=True)
         return data
     
 
